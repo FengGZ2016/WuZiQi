b779fcf14a01399a6572eaa9bccb46f9b1acab2c
冯国富 <fgz2016@outlook.com>
2017-06-08 10:37:10 +0800

    初始化项目

---
+++ app/src/main/java/com/example/wuziqi/MyView.java
@@ -0,0 +1,19 @@
+package com.example.wuziqi;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * 作者：国富小哥
+ * 日期：2017/6/8
+ * Created by Administrator
+ */
+
+public class MyView extends View{
+
+
+    public MyView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
