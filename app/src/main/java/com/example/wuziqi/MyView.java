79488a68b818b8b65c117fc732e4abd316bd56e5
冯国富 <fgz2016@outlook.com>
2017-06-08 11:07:36 +0800

    重写onMeasure方法

--- app/src/main/java/com/example/wuziqi/MyView.java
+++ app/src/main/java/com/example/wuziqi/MyView.java
@@ -15,5 +15,35 @@ public class MyView extends View{
 
     public MyView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        //View的背景颜色，易于区分布局
+        setBackgroundColor(0x44ff0000);
+    }
+
+    /**
+     * 当xml文件集成自定义View时调用此方法
+     * 该方法指定该控件在屏幕上的大小
+     * 第一：计算控件的实际大小
+     * 第二：设置控件的实际大小
+     * */
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        //通过辅助类获取size尺寸和mode模式
+        int widthSize=MeasureSpec.getSize(widthMeasureSpec);
+        int widthMode=MeasureSpec.getMode(widthMeasureSpec);
+
+        int heightSize=MeasureSpec.getSize(heightMeasureSpec);
+        int heightMode=MeasureSpec.getMode(heightMeasureSpec);
+        //当widthMode和heightMode为精确模式时
+        int width=Math.min(widthSize,heightSize);
+        //当widthMode和heightMode为未指定尺寸模式时
+        if (widthMode==MeasureSpec.UNSPECIFIED){
+            width=heightSize;
+        }else if (heightMode==MeasureSpec.UNSPECIFIED){
+            width=widthSize;
+        }
+
+        //调用setMeasuredDimension(int, int)设置实际大小
+        setMeasuredDimension(width,width);
     }
 }
